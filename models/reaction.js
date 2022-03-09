7e44f13d08e11ebc61045bb90081064ea272070c
lizbett <rlizbett21@gmail.com>
2022-03-09 12:06:37 -0600

    updated model files

--- models/reaction.js
+++ models/reaction.js
@@ -1,29 +1,21 @@
 const { Schema, model } = require('mongoose');
 
 // Schema to create User model
-const userSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    username: {type: String, require: true, unique: true, trim: true},
-    email: {type: String, require: true, unique: true, match: /.+\@.+\..+/ },
-    thoughts: [{ type: Schema.Types.ObjectId,ref: 'thought'}],
-    friends: [{ type: Schema.Types.ObjectId,ref: 'user'}] 
-},
+    reactionId: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId(), },
+    reactionBody: { type: String, required: true, maxlength: 25, },
+    createdAt: { type: Date, default: Date.now, },
+    username: { type: String, required: true, },
+  },
   {
     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
     // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
-      virtuals: true,
+      getters: true,
     },
     id: false,
   }
 );
 
-// Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('friendCount')
-  // Getter
-  .get(function () {
-    return this.friends.length;
-  })
-
-  module.exports = User;
+module.exports = reactionSchema;
--- models/thought.js
+++ models/thought.js
@@ -1,37 +1,33 @@
 const { Schema, model } = require('mongoose');
 
-function formatDate(date) {
-    date
-}
 // Schema to create User model
 const thoughtSchema = new Schema(
   {
     thoughtText: {type: String, require: true, minlength: 1, maxlength: 280},
-    createdAt: {type: Date, default: Date.now, get: date => formatDate(date)},
-    thoughts: [{ type: Schema.Types.ObjectId,ref: 'thought'}],
-    friends: [{ type: Schema.Types.ObjectId,ref: 'user'}] 
-},
+    createdAt: {type: Date, default: Date.now },
+    username: {type: String, required: true,},
+    reaction : [Reaction],
+ },
   {
     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
     // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
-      virtuals: true,
+      getters: true,
     },
     id: false,
   }
 );
 
 // Create a virtual property `fullName` that gets and sets the user's full name
-userSchema
-  .virtual('friendCount')
+thoughtSchema
+  .virtual('reactionCount')
   // Getter
   .get(function () {
-    return this.friends.length;
+    return this.reaction.length;
   })
 
 
 // Initialize our User model
 const thought = model('thought', thoughtSchema);
-
 
 module.exports = thought;
